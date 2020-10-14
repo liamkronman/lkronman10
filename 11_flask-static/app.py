89e9989c6e28f8c345a82c02f5760e48ef660a30
Liam Kronman <lkronman10@stuy.edu>
2020-10-13 22:12:09 -0400

    12_flask-jinja

--- 11_flask-static/app.py
+++ 11_flask-static/app.py
@@ -1,3 +1,8 @@
+Team Tissue (Maddy Andersen, Liam Kronman, Amelia Chin)
+SoftDev
+K11 - Some Things Never Change (experimenting with using flask to serve static files and making our own)
+2020-10-13
+
 # DEMO 
 # basics of /static folder
 
